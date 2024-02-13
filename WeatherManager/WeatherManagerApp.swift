1ab64645a819098990422ecfae429739a0960fc6
Austen Radigk <austenradigk@gmail.com>
2024-02-13 16:49:04 -0600

    Initial Commit

---
+++ WeatherManager/WeatherManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherManagerApp.swift
+//  WeatherManager
+//
+//  Created by Austen Radigk on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
